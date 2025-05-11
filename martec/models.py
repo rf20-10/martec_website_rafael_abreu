845e551fd9a14ada4402361ff75188d05de36960
Rafa Abreu <rafaabreu@Rafas-MacBook-Air.local>
2025-05-11 17:41:29 -0500

    Initial commit

--- martec/models.py
+++ martec/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # optional: add your custom fields here
+    # Feature for future autentication.
     pass
 
 # Model for products sold.
